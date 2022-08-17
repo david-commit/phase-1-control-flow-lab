3b2f2d594ce5ae93f7bbf41b2edcd585af3c624b
david-commit <david.ondiege@students.moringaschool.com>
2022-08-17 18:48:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet > 2500){
+    return 'No can do.';
+  } else if (feet >= 400){
+    return 'I will gladly take your thirty bucks.';
+  } else { 
+    return 'This one is on me!';
+ }}
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good.";
+  } else {
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return ('Bye.')
+  }
 }
